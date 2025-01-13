d7696ecf2a321deed363bae4b70359ca6929f302
geez14 <imonabbar@noreply.com>
2025-01-13 19:38:04 +0530

    implemented: resource creation end-point and pagination endpoint

--- src/main/java/com/geez14/app/controllers/CashCardController.java
+++ src/main/java/com/geez14/app/controllers/CashCardController.java
@@ -2,8 +2,15 @@ package com.geez14.app.controllers;
 
 import com.geez14.app.entities.CashCard;
 import com.geez14.app.repo.CashCardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController("CashCardAPI")
 @RequestMapping(path = "/cashcards")
@@ -29,11 +36,55 @@ public class CashCardController {
         java.util.Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
         // ResponseEntity.ok(cashCard.orElse(null)); fail test case
         // the below code is written by AI not me
-        // return cashCard.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        // return cashCard.map(ResponseEntity::ok).orElseGet(ResponseEntity.notFound()::build);
         // this is my code!
         if (cashCard.isPresent()) {
             return ResponseEntity.ok(cashCard.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+
+    /**
+     * CRUD {
+     * Operation: Write,
+     * EndPoint: /cashcards/,
+     * Body: {amount: xxx.xx},
+     * HTTPMethod: POST,
+     * }
+     *
+     * @param cashCard com.geez14.app.entities.CashCard
+     * @param ucb      org.springframework.web.util.UriComponentBuilder
+     * @return java.lang.Void
+     * Post mapping on /cashcards for creating new CashCard
+     * UriComponent was injected from our Spring IoC container.
+     * Because the official specification continues to state the following:
+     * > Send a 201 (created) response <b>containing a Location header field</b> that
+     * provides an identifier for the primary resource created
+     */
+    @PostMapping
+    private ResponseEntity<Void> create(@RequestBody CashCard cashCard, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(cashCard);
+        URI locator = ucb
+                .path("/cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locator).build();
+    }
+
+//    @GetMapping
+//    private ResponseEntity<Iterable<CashCard>> findAll() {
+//        return ResponseEntity.ok(cashCardRepository.findAll());
+//    }
+
+    @GetMapping
+    private ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.DESC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- src/main/java/com/geez14/app/repo/CashCardRepository.java
+++ src/main/java/com/geez14/app/repo/CashCardRepository.java
@@ -2,6 +2,7 @@ package com.geez14.app.repo;
 
 import com.geez14.app.entities.CashCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
