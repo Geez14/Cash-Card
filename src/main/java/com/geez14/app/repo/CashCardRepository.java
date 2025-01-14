681e74141ee56fb22146edb4394cfcf2410c956a
geez14 <imonabbar@noreply.com>
2025-01-14 15:17:30 +0530

    BreakingChanges: Added new functionality in repository

--- src/main/java/com/geez14/app/repo/CashCardRepository.java
+++ src/main/java/com/geez14/app/repo/CashCardRepository.java
@@ -1,8 +1,17 @@
 package com.geez14.app.repo;
 
 import com.geez14.app.entities.CashCard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    @Query("SELECT * from CASH_CARD WHERE ID=:cardNumber AND OWNER=:owner")
+    Optional<CashCard> findByIdAndOwner(Long cardNumber, String owner);
+
+    Page<CashCard> findAllByOwnerIgnoreCase(String owner, PageRequest pageRequest);
 }
