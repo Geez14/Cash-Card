1702ad85ac6bb66805236dcb5743bea3c28e4126
geez14 <imonabbar@noreply.com>
2025-01-20 18:42:47 +0530

    add: new query to delete all cashcards

--- src/main/java/com/geez14/app/repo/CashCardRepository.java
+++ src/main/java/com/geez14/app/repo/CashCardRepository.java
@@ -1,12 +1,15 @@
 package com.geez14.app.repo;
 
 import com.geez14.app.entities.CashCard;
+import jdk.jfr.BooleanFlag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
@@ -16,4 +19,7 @@ public interface CashCardRepository extends CrudRepository<CashCard, Long>, Pagi
     Page<CashCard> findAllByOwnerIgnoreCase(String owner, PageRequest pageRequest);
 
     boolean existsByIdAndOwner(Long requestedId, String name);
+
+    @Query("SELECT ID FROM CASH_CARD WHERE ID IN (:data) AND OWNER=:owner")
+    List<Long> getIdsByRequestedIdsAndOwner(Iterable<Long> data, String owner);
 }
